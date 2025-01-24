87ebafd73458bf6feb6fd5ba0fe1f7c31fb999e8
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2025-01-24 10:43:11 +0530

    update till delete item

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
+import Text from './components/Text.jsx';
+
 function App() {
   const [arr, setArr] = useState(['item1', 'item2', 'item3']);
   const [inputValue, setInputValue] = useState('');
+
   console.log(inputValue);
   console.log(arr);
+
   const handleChange = (event) => {
     setInputValue(event.target.value);
   };
@@ -12,8 +16,16 @@ function App() {
     event.preventDefault();
     setArr([...arr, inputValue]);
   };
+
+  const handleDelete = (itemdelete) => {
+    const updatedItems = arr.filter((item) => {
+      return item !== itemdelete;
+    });
+    setArr(updatedItems);
+  };
   return (
     <div>
+      <Text />
       <form onSubmit={handleSubmit}>
         <input
           onChange={handleChange}
@@ -28,7 +40,15 @@ function App() {
         </button>
       </form>
       {arr.map((item) => (
-        <h1>{item}</h1>
+        <div className="flex m-2 gap-2">
+          <span>{item}</span>
+          <button
+            onClick={() => handleDelete(item)}
+            className="bg-red-700 text-white px-6 py-2 hover:bg-red-400 rounded-xl"
+          >
+            Delete
+          </button>
+        </div>
       ))}
     </div>
   );
---
+++ src/components/Text.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Text() {
+  return (
+    <div className='p-10 border border-red-500 m-10'>
+     <h2>Rendering</h2> 
+    </div>
+  )
+}
+
+export default Text
