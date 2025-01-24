80f10db678e6fa8767e907375e21d2a4ff90e022
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2025-01-24 10:17:34 +0530

    updated till array

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
-
+import { useState } from 'react';
 function App() {
-  // const companies = ['meta' , 'google' ,'neflix']
-  const companies = [
-    { id: 1, name: 'meta' },
-    { id: 2, name: 'google' },
-    { id: 3, name: 'netflix' },
-  ];
+  const [arr, setArr] = useState(['item1', 'item2', 'item3']);
+  const [inputValue, setInputValue] = useState('');
+  console.log(inputValue);
+  console.log(arr);
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setArr([...arr, inputValue]);
+  };
   return (
     <div>
-      <h1 className="text-center text-blue-500 font-bold text-4xl">
-        List rendering
-      </h1>
-      {/* <h1>{companies[0]}</h1>
-      <h1>{companies[1]}</h1>
-      <h1>{companies[2]}</h1> */}
-      {companies.map((company, index) => (
-        <h1 key={company.id}>{company.name}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          className="border border-slate-800 rounded-lg w-64 p-2 mr-2"
+          type="text"
+        />
+        <button
+          className="bg-sky-500 px-10 py-2 rounded-xl text-white"
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+      {arr.map((item) => (
+        <h1>{item}</h1>
       ))}
     </div>
   );
